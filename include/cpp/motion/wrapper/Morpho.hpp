b73364e501a5aa28d3d03a182acb0f4dd2cf33ce
aliasgwynplaine <marco.leon@etu.sorbonne-universite.fr>
2025-11-17 18:55:44 +0100

    morpho: not working

---
+++ include/cpp/motion/wrapper/Morpho.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "motion/morpho/morpho_compute.h"
+#include "motion/morpho/morpho_struct.h"
+
+class Morpho : public spu::module::Stateful {
+protected:
+	int i0;
+	int i1;
+	int j0;
+	int j1;
+	uint8_t 
+
+}
