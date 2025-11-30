d9776019b1a20f29aa051cb613f471152ceb0949
gwynplaine <mnleonr@uni.pe>
2025-11-30 16:37:06 +0100

    fix: virutal destructor in Features_CCA

--- include/cpp/motion/wrapper/Features_CCA.hpp
+++ include/cpp/motion/wrapper/Features_CCA.hpp
@@ -11,5 +11,5 @@ protected:
         const size_t max_sz;
 public:
         Features_CCA(const int i0, const int i1, const int j0, const int j1, const size_t max_RoIs_size);
-        virtual ~Features_CCA();
+        virtual ~Features_CCA() = default; // todo: verify
 };
