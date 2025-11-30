01197c2e04aaea72a25808e874d29b508f7a9e2b
lailabennani <laila.lahkim.bennani03@gmail.com>
2025-11-30 16:22:43 +0100

    added Sigma_delta modules (not 100% sure theyre working ^^)

--- include/cpp/motion/wrapper/Sigma_delta.hpp
+++ include/cpp/motion/wrapper/Sigma_delta.hpp
@@ -1,21 +1,25 @@
-
-
 #pragma once
 
-#include <stdint.h>
+#include <stdlib.h>
+#include <nrc2.h>
+#include <math.h>
+
 #include <streampu.hpp>
-#include "motion/sigma_delta_compute.h"
+#include "motion/sigma_delta.h"
 
-class Sigma_delta : public::module::Stateful {
+class Sigma_delta : public spu::module::Stateful {
 protected:
-	const int i0;
-	const int i1;
-	const int j0;
-	const int j1;
-	const sigma_delta_data_t* sd_data;
+	int i0;
+	int i1;
+	int j0;
+	int j1;
+	uint8_t vmin;
+	uint8_t vmax;
+	sigma_delta_data_t* sd_data;
 
+	uint8_t **tmp_img; // not sure yet
 public:
-	Sigma_delta(const int i0, const int i1, const int j0, const int j1, const uint8_t vmin, const uint8_t vmax )
+	Sigma_delta(const int i0, const int i1, const int j0, const int j1, const uint8_t vmin, const uint8_t vmax);
+	virtual ~Sigma_delta();
 
-
-}
+};
--- src/wrapper/Sigma_delta.cpp
+++ src/wrapper/Sigma_delta.cpp
@@ -1,3 +1,35 @@
 #include "motion/wrapper/Sigma_delta.hpp"
 
+Sigma_delta::Sigma_delta(const int i0, const int i1, const int j0, const int j1, const uint8_t vmin, const uint8_t vmax) : spu::module::Stateful(), i0(i0), i1(i1), j0(j0), j1(j1), vmin(vmin), vmax(vmax){
+	
+	const std::string name = "sigma_delta";
+	this->set_name(name);
+	this->set_short_name(name);
+	this->sd_data = sigma_delta_alloc_data(i0, i1, j0, j1, vmin, vmax);
+	sigma_delta_init_data(this->sd_data, NULL, this->i0, this->i1, this->j0, this->j1); // laila said check this
 
+	auto &t = this->create_task("compute");
+	auto si_img = this->template create_2d_socket_in<uint8_t>(t, "in_img", (i1-i0)+1, (j1-j0)+1); //marcosaidgrayscale
+	auto so_img = this->template create_2d_socket_out<uint8_t>(t, "out_img", (i1-i0)+1, (j1-j0)+1); //marcosaidgrayscale
+	
+	this->create_codelet(t,
+		[si_img, so_img]
+			(spu::module::Module &m, spu::runtime::Task &p, const size_t frame_id) -> int {
+		auto &sd = static_cast<Sigma_delta&>(m);
+
+		const uint8_t **in_img = p[si_img].get_2d_dataptr<const uint8_t>();
+		zero_ui8matrix(sd.tmp_img, sd.i0, sd.i1, sd.j0, sd.j1);
+		uint8_t **out_img = p[so_img].get_2d_dataptr<uint8_t>();
+
+		sigma_delta_compute(sd.sd_data, in_img, out_img, sd.i0, sd.i1, sd.j0, sd.j1, 1); //VERY IMPORTANT : IDK WHAT N MEANS AND WHAT TO PUT HERE
+
+		return spu::runtime::status_t::SUCCESS;
+	});
+
+}
+
+Sigma_delta::~Sigma_delta(){
+	sigma_delta_free_data(this->sd_data);
+	free_ui8matrix(this->tmp_img, this->i0, this->i1, this->j0, this->j1);
+	
+}
