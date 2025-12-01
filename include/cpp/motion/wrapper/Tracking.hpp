90b5acf9aa1c1a5c542bef7cc90df1383e2ea16b
gwynplaine <mnleonr@uni.pe>
2025-12-01 20:02:10 +0100

    rename: Tracker -> Tracking

--- include/cpp/motion/wrapper/Tracking.hpp
+++ include/cpp/motion/wrapper/Tracking.hpp
@@ -19,7 +19,11 @@ protected:
 	tracking_data_t *tracking_data;
 
 public:
-	Tracking(const size_t r_extrapol, const size_t fra_obj_min, const uint8_t save_RoIs_id, const uint8_t extrapol_order_max, const float min_extrapol_ratio_S, const size_t max_history_size, const size_t max_RoIs_size);
+	Tracking(const size_t r_extrapol, const size_t fra_obj_min, const uint8_t save_RoIs_id, const uint8_t extrapol_order_max,
+		 const float min_extrapol_ratio_S, const size_t max_history_size, const size_t max_RoIs_size);
 
 	~Tracking();
+
+	tracking_data_t *get_tracking_data() const;
 };
+
--- src/wrapper/Tracking.cpp
+++ src/wrapper/Tracking.cpp
@@ -1,4 +1,4 @@
-#include "motion/wrapper/Tracker.hpp"
+#include "motion/wrapper/Tracking.hpp"
 
 Tracking::Tracking(const size_t r_extrapol, const size_t fra_obj_min, const uint8_t save_RoIs_id, const uint8_t extrapol_order_max, const float min_extrapol_ratio_S, const size_t max_history_size, const size_t max_RoIs_size)
 : spu::module::Stateful(), r_extrapol(r_extrapol), fra_obj_min(fra_obj_min), save_RoIs_id(save_RoIs_id), extrapol_order_max(extrapol_order_max), min_extrapol_ratio_S(min_extrapol_ratio_S), max_history_size(max_history_size), max_RoIs_size(max_RoIs_size)
@@ -23,7 +23,16 @@ Tracking::Tracking(const size_t r_extrapol, const size_t fra_obj_min, const uint
 			const RoI_t* in_RoIs = p[si_RoIs].get_dataptr<const RoI_t>();
 			const uint32_t in_n_RoIs = p[si_n_RoIs].get_dataptr<const uint32_t>()[0];
 			
-			tracking_perform(track.tracking_data, in_RoIs, in_n_RoIs, frame_id, track.r_extrapol, track.fra_obj_min, track.save_RoIs_id, track.extrapol_order_max, track.min_extrapol_ratio_S);
+			tracking_perform(track.tracking_data, 
+				in_RoIs, 
+				in_n_RoIs, 
+				frame_id, 
+				track.r_extrapol, 
+				track.fra_obj_min, 
+				track.save_RoIs_id, 
+				track.extrapol_order_max, 
+				track.min_extrapol_ratio_S
+			);
 
 			return spu::runtime::status_t::SUCCESS;		
 	});
@@ -32,4 +41,8 @@ Tracking::Tracking(const size_t r_extrapol, const size_t fra_obj_min, const uint
 
 Tracking::~Tracking(){
 	tracking_free_data(this->tracking_data);
+}
+
+tracking_data_t *Tracking::get_tracking_data() const {
+    return this->tracking_data;
 }
